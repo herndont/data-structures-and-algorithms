de7a5bbd64ca8b64d7701be60deec927d545e3ce
herndont <herndontrey@gmail.com>
2019-04-21 13:36:25 -0700

    changes made to get full credit

--- challenges/array_reverse/ArrayReverse.java
+++ challenges/array_reverse/ArrayReverse.java
@@ -3,8 +3,17 @@ import java.util.Arrays;
 public class ArrayReverse {
   public static void main(String[] args){
         int [] originalArray = {
-      1, 2, 3, 4, 5, 6};
-      reverseArray(originalArray);
+                1, 2, 3, 4, 5, 6};
+        int [] array2 = {
+                89, 2354, 3546, 23, 10, -923, 823, -12
+        }
+        int [] array3 = {
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+                103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
+        }
+        reverseArray(originalArray);
+        reverseArray(array2);
+        reverseArray(array3);
   }
   public static int[] reverseArray(int[] originalArray){
           int temp;
--- challenges/array_reverse/README.md
+++ challenges/array_reverse/README.md
@@ -39,5 +39,5 @@ My approach was to reverse the array in place. I did this by introducing the var
 As stated in class we are not referencing the Big 0 yet, so I will leave this for the next challenge.
 
 ## Solution
-<img src="assets\array_reverse.jpg" alt="white board solution">
+[White Board Image](../assets/array_reverse.jpg)
 
